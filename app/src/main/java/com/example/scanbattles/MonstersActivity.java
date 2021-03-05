8c2570264ce510f1bcce6895480a1383a126df08
Ben Stratford <stratfob@tcd.ie>
2021-03-05 15:20:43 +0000

    Added ability to change monster team

--- app/src/main/java/com/example/scanbattles/MonstersActivity.java
+++ app/src/main/java/com/example/scanbattles/MonstersActivity.java
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -24,6 +26,7 @@ public class MonstersActivity extends AppCompatActivity implements MonsterAdapte
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
     ArrayList<Monster> myMonsters;
+    int selectedTeam = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,14 +53,46 @@ public class MonstersActivity extends AppCompatActivity implements MonsterAdapte
         return true;
     }
 
-    public void setTeam(int team){
-
+    public void setTeam(int monsterPosition, int team){
+        Monster monster = myMonsters.get(monsterPosition);
+        monster.team = team;
+        AppDatabase.getAppDatabase(this).monsterDao().update(monster);
+        mAdapter.notifyDataSetChanged();
     }
 
     @Override
     public void onMonsterClick(int position) {
-        Toast toast = Toast.makeText(getApplicationContext(), myMonsters.get(position).name, Toast.LENGTH_SHORT);
-        toast.show();
+//        Toast toast = Toast.makeText(getApplicationContext(), myMonsters.get(position).name, Toast.LENGTH_SHORT);
+//        toast.show();
+        String monsterName = myMonsters.get(position).name;
+        showOptionsDialogue(position, monsterName);
+    }
+
+    private void showOptionsDialogue(final int monsterPosition, String monsterName) {
+        selectedTeam = 0;
+        final String[] teamChoices = new String[] {"None", "Team 1", "Team 2", "Team 3"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(MonstersActivity.this);
+        builder.setTitle("Choose a team for " + monsterName);
+        builder.setSingleChoiceItems(teamChoices, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                selectedTeam = which;
+            }
+        });
+        builder.setPositiveButton("Select", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                setTeam(monsterPosition, selectedTeam);
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("Back", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
     }
 }
 
